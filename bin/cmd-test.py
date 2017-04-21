3667488d598e32c3f70225bf7b9f6f1fddf2b768
Mortezaipo <mortezaipo@gmail.com>
2017-04-21 13:57:22 +0430

    Create cmd-test main file to run command.

--- bin/cmd-test.py
+++ bin/cmd-test.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
                         .CMD TEST.
 
@@ -13,13 +14,41 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+
 import sys
+from src import lib
+from src import show
+from src import const
+from src import dispatch
 
 
-def main(args):
-    """Main method to start tests."""
-    pass
+def main(args: list):
+    """Main method to start tests.
 
+    :param args: list of entered arguments
+    :type args: list
+    :return: void
+    :rtype: void
+    """
+    if not lib.is_file_exists(args[0]):
+        show.fprint(const.ERROR, 2, "Config file not found.")
+
+    if not lib.has_file_read_permission(args[0]):
+        show.fprint(const.ERROR, 3, "Config file doesn't have read permission.")
+
+    config = lib.load_config_file(args[0])
+    if not config:
+        show.fprint(const.ERROR, 4, "Config file content is not valid.")
+
+    check_result = lib.validate_config_structure(config)
+    if check_result is not True:
+        show.fprint(const.ERROR, -1, "Config file structure is not valid.")
+        show.fprint(const.ERROR, 5, "***ERROR***", check_result[1])
+
+    show.fprint(const.INFO, -1, "cmd-test started...")
+    lib.dispatch.start(config)
 
 if __name__ == "__main__":
+    if len(sys.args) == 0:
+        fprint(consts.ERROR, 1, "Need config file address as first parameter.")
     main(sys.args)
